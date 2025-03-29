1b60373ed4895399cb50a829713f75aee5556476
Raouia <raouiaabourachid@gmail.com>
2025-03-29 00:36:24 +0000

    add comments

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App />  
+    {/*   This is the main component of your application */}
   </StrictMode>,
 )
